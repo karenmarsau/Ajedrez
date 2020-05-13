a7075def74f9f3b02b369ecc8550b0df22de0bf7
karenmarsau <cf19karen.gomez@iesjoandaustria.org>
2020-05-13 11:26:03 +0200

    Ajedrez Push1

--- app/src/main/java/com/example/ajedrez/MainActivity.java
+++ app/src/main/java/com/example/ajedrez/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ajedrez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnJugar = findViewById(R.id.btnJugar);
+        btnJugar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goToOpcionesJuego();
+            }
+        });
+
+    }
+    private void goToOpcionesJuego(){
+        Intent intentOpciones = new Intent(this,OpcionesJuego.class);
+        startActivityForResult(intentOpciones, 1);
     }
 }
